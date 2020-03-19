d4d630ca136e6b4e4276f32383308b2ca5481cbc
motokikasai <kmomoky@hotmail.com>
2020-03-19 12:52:30 +0100

    Add Get localStorage

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const header = document.querySelector(".header");
 const container = document.querySelector(".container");
 
 // GET Local Storage onload...
+if (localStorage.getItem("checkbox") == "true") {
+  checkbox.checked = true;
+  container.classList.add("container-dark");
+  header.classList.add("header-dark");
+} else {
+  container.classList.remove("container-dark");
+  header.classList.remove("header-dark");
+}
 
 const switcher = () => {
   if (checkbox.checked) {
